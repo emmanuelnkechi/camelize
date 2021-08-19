1b3f0de9dee3fd51174ba1832c32f2ffacfd9ac4
Nkechi Emmanuel <nkechiemmanuel95@gmail.com>
2021-08-19 12:53:19 +0100

    added a default export

--- camelize/src/main.js
+++ camelize/src/main.js
@@ -1,12 +1,4 @@
 import App from './App.svelte';
- import Camelize from "./Camelize.svelte";
+import Camelize from "./Component/Camelize.svelte"
+
 export default Camelize;
-
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
-
-export default app;
