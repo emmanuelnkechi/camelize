01d2aef4f38f2a96b131651caba5f5a761a18c68
Ayo Alfonso <ayo@lynchpyn.com>
2021-08-23 18:37:18 +0100

    Add the new rollup config

--- src/Component/Camelize.svelte
+++ src/Component/Camelize.svelte
@@ -1,16 +1,16 @@
 <script>
-export let word;
-  let answer = function toCamelCase(str = word) {
-    return str
-      .split(" ")
-      .map(function (word, index) {
-        if (index == 0) {
-          return word.toLowerCase();
-        }
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-      })
-      .join("");
-  }()
+        export let word;
+        let answer = function toCamelCase(str = word) {
+        return str
+          .split(" ")
+          .map(function (word, index) {
+            if (index == 0) {
+              return word.toLowerCase();
+            }
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+          })
+          .join("");
+        }()
 </script>
 
 <p>{answer}</p>
